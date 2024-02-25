a669af025f45e45bd3ad3544fc51e91ff0d6dc08
huntertdiamond <huntertdiamond@gmail.com>
2024-02-24 22:14:50 -0500

    resolved header layout

--- framegear/app/page.tsx
+++ framegear/app/page.tsx
@@ -6,9 +6,9 @@ import { ValidationResults } from '@/components/ValidationResults';
 
 export default function Home() {
   return (
-    <div className="max-w-layout-max mx-auto flex flex-col gap-8 pb-16">
+    <div className="max-w-layout-max mx-auto  flex flex-col items-center gap-8 pb-16">
       <Header />
-      <div className="mx-auto flex w-full gap-8">
+      <div className="max-w-layout-max mx-auto flex gap-8">
         <div className="flex w-1/3 flex-col gap-4">
           <FrameInput />
           <Frame />
